05e5d431d154a7118b53684a4ad480ded51d93c8
Atsuya Yamagami <my2502@icloud.com>
2024-10-26 23:46:27 +0900

    refactor: Prettierを実行 (#1)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import jsConfig from "@eslint/js";
-import prettierConfig from "eslint-config-prettier";
-import unusedImportsConfig from "eslint-plugin-unused-imports";
+import { FlatCompat } from '@eslint/eslintrc';
+import jsConfig from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import unusedImportsConfig from 'eslint-plugin-unused-imports';
 
 const compat = new FlatCompat();
 
@@ -10,41 +10,35 @@ const compat = new FlatCompat();
 /** @type {import('eslint').Linter.Config} */
 const config = [
   jsConfig.configs.recommended,
-  ...compat.extends("next/core-web-vitals"),
-  ...compat.extends("next/typescript"),
+  ...compat.extends('next/core-web-vitals'),
+  ...compat.extends('next/typescript'),
   {
     rules: {
-      "import/order": [
-        "warn",
+      'import/order': [
+        'warn',
         {
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            ["parent", "sibling", "index"],
-            "object",
-          ],
-          "newlines-between": "always",
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index'], 'object'],
+          'newlines-between': 'always',
           alphabetize: {
-            order: "asc",
+            order: 'asc',
           },
         },
       ],
-      "import/consistent-type-specifier-style": ["warn", "prefer-inline"],
+      'import/consistent-type-specifier-style': ['warn', 'prefer-inline'],
     },
   },
   {
     plugins: {
-      "unused-imports": unusedImportsConfig,
+      'unused-imports': unusedImportsConfig,
     },
     rules: {
-      "unused-imports/no-unused-imports": "warn",
+      'unused-imports/no-unused-imports': 'warn',
     },
   },
   prettierConfig,
   {
     linterOptions: {
-      reportUnusedDisableDirectives: "error",
+      reportUnusedDisableDirectives: 'error',
     },
   },
 ];
