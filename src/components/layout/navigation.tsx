cc863e0e8a3e2cd21c4824858d81f8754c10e0b7
片岡勇輝 <kayu@kataokayuukinoMacBook-Pro.local>
2025-01-20 09:29:03 +0900

    :art: 不要なコンポーネントを削除しロゴにトップページへのリンクを追加

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -11,7 +11,6 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
-import { cn } from '@/lib/cn';
 
 import { ModeToggle } from './mode-toggle';
 import logo from '../../../public/seminar-logo.svg';
@@ -35,7 +34,9 @@ export function Navigation() {
   return (
     <header className="border-b">
       <div className="mx-auto flex h-16 max-w-7xl items-center px-4">
-        <Image src={logo} width={64} height={48} alt="ゼミロゴ" />
+        <Link href="/" legacyBehavior passHref>
+          <Image src={logo} width={64} height={48} alt="ゼミロゴ" className="cursor-pointer" />
+        </Link>
         <NavigationMenu className="ml-6">
           <NavigationMenuList>
             {PAGES.map(({ name, path }) => (
@@ -56,26 +57,3 @@ export function Navigation() {
     </header>
   );
 }
-
-const ListItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(
-  ({ className, title, children, ...props }, ref) => {
-    return (
-      <li>
-        <NavigationMenuLink asChild>
-          <a
-            ref={ref}
-            className={cn(
-              'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-              className,
-            )}
-            {...props}
-          >
-            <div className="text-sm font-medium leading-none">{title}</div>
-            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
-          </a>
-        </NavigationMenuLink>
-      </li>
-    );
-  },
-);
-ListItem.displayName = 'ListItem';
